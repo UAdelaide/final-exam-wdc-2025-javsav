04aeba757b8170672b3d80da0f9e0b7535db5256
javsav <javed.alam@student.adelaide.edu.au>
2025-06-20 11:59:00 +0000

    [automatic commit] 2025-06-20 11:59:00 UTC 103.137.12.148

--- part1/db.js
+++ part1/db.js
@@ -112,7 +112,8 @@ module.exports = (async () => {
   ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
   ((SELECT dog_id FROM Dogs WHERE name = 'Diesel'), '2025-06-25 12:00:00', 60, 'Norwood', 'open'),
   ((SELECT dog_id FROM Dogs WHERE name = 'Woggy'), '2025-06-22 09:00:00', 30, 'Burnside', 'open'),
-  ((SELECT dog_id FROM Dogs WHERE name = 'Stinky'), '2025-06-21 12:35:00', 25, 'Glenelg', 'accepted');
+  ((SELECT dog_id FROM Dogs WHERE name = 'Stinky'), '2025-06-21 12:35:00', 25, 'Glenelg', 'accepted'),
+  (2, '2025-06-21 12:35:00')
       `);
 
      // Insert walkapplication data
