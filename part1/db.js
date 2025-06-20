ef23ffce75028943eeec7c375c42a949457a9bc4
javsav <javed.alam@student.adelaide.edu.au>
2025-06-20 12:01:21 +0000

    [automatic commit] 2025-06-20 12:01:21 UTC 103.137.12.148

--- part1/db.js
+++ part1/db.js
@@ -109,7 +109,7 @@ module.exports = (async () => {
     await pool.execute(`
       INSERT into WalkRequests (dog_id, requested_time, duration_minutes, location, status)
   VALUES ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-  ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+  ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'completed'),
   ((SELECT dog_id FROM Dogs WHERE name = 'Diesel'), '2025-06-25 12:00:00', 60, 'Norwood', 'open'),
   ((SELECT dog_id FROM Dogs WHERE name = 'Woggy'), '2025-06-22 09:00:00', 30, 'Burnside', 'completed'),
   ((SELECT dog_id FROM Dogs WHERE name = 'Stinky'), '2025-06-21 12:35:00', 25, 'Glenelg', 'completed');
