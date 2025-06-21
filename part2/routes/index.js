e692ed4d2b56c3bcab2c4fe44d7976454d2403d4
javsav <javed.alam@student.adelaide.edu.au>
2025-06-21 06:28:16 +0000

    [automatic commit] 2025-06-21 06:28:16 UTC 103.137.12.53

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -41,13 +41,15 @@ module.exports = function(pool) {
   /* Returns a list of all dogs with their size and owner name */
 router.get('/api/dogs', async function(req, res, next) {
   try {
-    // Query databaes for dogs
+    // Query databaes for dog details and owners names
   const [result] = await pool.query(
         'SELECT Dogs.name as dog_name, Dogs.size, Users.username as owner_username FROM Dogs JOIN Users on Users.user_id = Dogs.owner_id'
   );
+  // Send the results as json
   res.json({
     dogs: result
   });
+  // Cacth ay errors
 } catch (error) {
   console.error('Error retrieving list of dogs from database: ' + error);
   res.status(500).json({ message: 'Internal server error when fetching dog list.' });
