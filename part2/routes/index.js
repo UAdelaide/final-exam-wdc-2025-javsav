5caae75b0e97189b80b9d57bfc42139c851037a8
javsav <javed.alam@student.adelaide.edu.au>
2025-06-21 05:02:27 +0000

    [automatic commit] 2025-06-21 05:02:27 UTC 103.137.12.53

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -38,5 +38,22 @@ module.exports = function(pool) {
   }
   });
 
+  /* Returns a list of all dogs with their size and owner name */
+router.get('/api/dogs', async function(req, res, next) {
+  try {
+
+  const [result] = await pool.query(
+        'SELECT Dogs.name as dog_name, Dogs.size, Users.username as owner_username FROM Dogs JOIN Users on Users.user_id = Dogs.owner_id'
+  );
+  res.json({
+    dogs: result
+  });
+} catch (error) {
+  console.error('Error retrieving list of dogs from database: ' + error);
+  res.status(500).json({ message: 'Internal server error when fetching dog list.' });
+}
+});
+
+
   return router;
 };
